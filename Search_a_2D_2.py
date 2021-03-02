0ca482d220f83c3cfca2a61a697b544e922901b2
jsk1107 <seungkyu.korea@gmail.com>
2021-03-03 00:50:01 +0900

    update

---
+++ Search_a_2D_2.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Author: jsk1107
+
+"""
+Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+"""
+
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import *
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        m, n = len(matrix), len(matrix[0])
+
+        pivot_row, pivot_col = 0, n - 1
+
+        while pivot_row < m and pivot_col >= 0:
+            if matrix[pivot_row][pivot_col] < target:
+                pivot_row += 1
+            elif matrix[pivot_row][pivot_col] > target:
+                pivot_col -= 1
+            else:
+                return True
+        return False
+
+if __name__ == '__main__':
+    matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24],
+              [18, 21, 23, 26, 30]]
+    target = 35
+    solution = Solution()
+    output = solution.searchMatrix(matrix, target)
+    print(output)
+
+"""
+
+"""
---
+++ Set Matrix Zeroes.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Author: jsk1107
+
+"""
+Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+Follow up:
+
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+"""
+
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import *
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        m, n = len(matrix), len(matrix[0])
+        zero_col = []
+        inp = False
+
+        for i in range(m):
+            if 0 in matrix[i]:
+                index = [idx for idx, value in enumerate(matrix[i]) if value == 0]
+                zero_col.extend(index)
+                matrix[i] = [0] * n
+                inp = True
+
+        if not inp:
+            return matrix
+
+        zero_col = list(set(zero_col))
+        for i in zero_col:
+            for j in range(m):
+                matrix[j][i] = 0
+        return matrix
+
+
+if __name__ == '__main__':
+    matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+    solution = Solution()
+    output = solution.setZeroes(matrix)
+    print(output)
+
+"""
+
+"""
